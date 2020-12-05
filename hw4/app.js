9c2b652be188284f2b1baf7c1a58e6c6c7b982f1
josejr-coder <70986835+josejr-coder@users.noreply.github.com>
2020-12-04 20:44:13 -0800

    Update app.js

--- hw4/app.js
+++ hw4/app.js
@@ -11,30 +11,45 @@ registration.scope);
 } else {
  console.log("Service Worker is not supported by this browser");
 }
-
-//push event
-self.addEventListener('push', function(event) {
-  if (event.data) {
-    console.log('This push event has data: ', event.data.text());
-  } else {
-    console.log('This push event has no data.');
-  }
-});
 //app
-const webpush = require('web-push');
-const vapidKeys = webpush.generateVAPIDKeys();
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
 
-const options = {
-    TTL: 24*60*60, //TTL= time notification will be queued in the push service
-    vapidDetails: {
-        subjec : 'email@example.com',
-        publicKey: '',
-        privateKey: ''
+let buttonInstall = document.querySelector('.bapp');
+
+buttonInstall.addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  hideMyInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
     }
-}
-const data = {
-    title: 'Update',
-    body: 'Notification sent by server'
-}
-
-webpush.sendNotification(subscription, data, options)
+  });
+});
+window.addEventListener('appinstalled', (evt) => {
+  // Log install to analytics
+  console.log('INSTALL: Success');
+});
+window.addEventListener('DOMContentLoaded', () => {
+  let displayMode = 'browser tab';
+  if (navigator.standalone) {
+    displayMode = 'standalone-ios';
+  }
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    displayMode = 'standalone';
+  }
+  // Log launch display mode to analytics
+  console.log('DISPLAY_MODE_LAUNCH:', displayMode);
+});
