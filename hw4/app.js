805c96b15b8029553d92f7c511655dfc747fee8f
josejr-coder <70986835+josejr-coder@users.noreply.github.com>
2020-12-04 17:54:15 -0800

    Add files via upload

---
+++ hw4/app.js
@@ -0,0 +1,32 @@
+if (navigator.serviceWorker) {
+ navigator.serviceWorker.register("./service-worker.js", {scope: "./"})
+ .then(function (registration) {
+ // Registration of service worker ok
+ console.log("Registration of service worker successful with scope: ",
+registration.scope);
+ }).catch(function(err) {
+ // Registration of service worker failed
+ console.log("Registration of service worker failed with error: ", err);
+ })
+} else {
+ console.log("Service Worker is not supported by this browser");
+}
+
+//app
+const webpush = require('web-push')
+const vapidKeys = webpush.generateVAPIDKeys()
+
+const options = {
+    TTL: 24*60*60, //TTL= time notification will be queued in the push service
+    vapidDetails: {
+        subjec : 'email@example.com',
+        publicKey: '',
+        privateKey: ''
+    }
+}
+const data = {
+    title: 'Update',
+    body: 'Notification sent by server'
+}
+
+webpush.sendNotification(subscription, data, options)
